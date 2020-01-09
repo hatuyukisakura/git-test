4a9b3c9c64e77802f0078b8d8d17dae663ff77e6
kutori <731905912@qq.com>
2020-01-09 21:00:35 +0800

    add test

---
+++ lucene_first/src/cn/itcast/lucene/test.java
@@ -0,0 +1,9 @@
+package cn.itcast.lucene;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello github");
+        int i = 2;
+        System.out.println(++i);
+    }
+}
